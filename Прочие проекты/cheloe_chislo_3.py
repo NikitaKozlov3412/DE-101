c3c3443503ea9f5456c908e2b5ae113fbb8926b0
NikitaKozlov3412 <158044917+NikitaKozlov3412@users.noreply.github.com>
2024-02-19 22:53:40 +0300

    Add files via upload

---
+++ Прочие проекты/cheloe_chislo_3.py
@@ -0,0 +1,27 @@
+from array import *
+my_list = []
+i = 0
+print ("Введите целое число")
+n = int(input())
+summa = 0
+while n != 0:
+    k = abs(n)
+    m = n
+    s = 0
+    while k > 0:
+        s = s + k%10
+        k //= 10
+    if s > summa:
+        summa = s
+        maximum = m
+        my_list.clear()
+    elif s == summa:
+       drugoe = m
+       my_list.append(drugoe)
+    print ("Введите другое число")
+    n = int(input())
+print ("Вы ввели 0")
+print ("Число", maximum, "с наибольшей суммой цифр:", summa)
+if len(my_list) > 0:
+    print ("Другие числа",   "с такой же наибольшей суммой цифр:")
+    print(*my_list, sep=', ')
