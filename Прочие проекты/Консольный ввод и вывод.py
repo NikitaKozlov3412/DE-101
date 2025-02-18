1da8bcc8edf1b0d3a5aca6d2449b7c8bef63291b
NikitaKozlov3412 <158044917+NikitaKozlov3412@users.noreply.github.com>
2025-02-18 22:20:17 +0300

    Add files via upload

---
+++ Прочие проекты/Консольный ввод и вывод.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 24 00:48:45 2024
+
+@author: Администратор
+"""
+
+# Вывод на консоль
+
+print("Hello World", end=" ")
+print("Hello METANIT.COM", end=" ")
+print("Hello Python")
+
+print("Hello World", end=" and ")
+print("Hello METANIT.COM", end=" and ")
+print("Hello Python")
+
+# Консольный ввод
+
+name = input("Введите свое имя: ")
+print(f"Ваше имя: {name}")
+
+name = input("Your name: ")
+age = input("Your age: ")
+print(f"Name: {name}  Age: {age}")
