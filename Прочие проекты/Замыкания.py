cc571f34c6ea2524c725b2bed26da61af957e9b2
NikitaKozlov3412 <158044917+NikitaKozlov3412@users.noreply.github.com>
2025-03-06 22:12:10 +0300

    Add files via upload

---
+++ Прочие проекты/Замыкания.py
@@ -0,0 +1,50 @@
+# Замыкания
+
+def outer():        # внешняя функция
+    n = 5           # лексическое окружение - локальная переменная
+ 
+    def inner():      # локальная функция
+        nonlocal n
+        n += 1        # операции с лексическим окружением
+        print(n)
+ 
+    return inner
+ 
+ 
+fn = outer()   # fn = inner, так как функция outer возвращает функцию inner
+# вызываем внутреннюю функцию inner
+fn()    # 6
+fn()    # 7
+fn()    # 8
+
+print("\n")
+
+
+# Применение параметров
+
+def multiply(n):
+    def inner(m): return n * m
+ 
+    return inner
+ 
+ 
+fn = multiply(5)
+print(fn(5))        # 25
+print(fn(6))        # 30
+print(fn(7))        # 35
+
+print("\n")
+
+
+# Также можно было бы сократить этот код с помощью лямбд:
+    
+def multiply(n): return lambda m: n * m
+ 
+ 
+fn = multiply(5)
+print(fn(5))        # 25
+print(fn(6))        # 30
+print(fn(7))        # 35
+
+print("\n")
+
