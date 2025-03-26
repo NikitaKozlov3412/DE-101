c66300ef77e74ff08edd2719e2db9911daa4e28f
NikitaKozlov3412 <158044917+NikitaKozlov3412@users.noreply.github.com>
2025-03-26 22:59:07 +0300

    Add files via upload

---
+++ Прочие проекты/Список в обратном порядке.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 13 21:04:24 2025
+
+@author: Администратор
+"""
+
+sp = [10, 20, 50, 40, 30]
+
+sp2 = sp.copy()
+
+#sp2.sort()
+sp2.reverse()
+
+print("Начальный список: ", sp)
+print("Список в обратном порядке: ", sp2)
